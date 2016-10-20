a0e915a5e8b2cb6f8e4ce0fc6d742a80fe606150
Mike James <u1015123@umail.utah.edu>
2016-10-20 16:55:51 -0600

    Mike: new Core

--- Core.v
+++ Core.v
@@ -50,22 +50,86 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Core(
-    input [2:0] opcode,
-    input [2:0] regA,
-    input [2:0] regB,
-    input [2:0] regC,
-    output instrComplete,
-    input [15:0] dInImmediate, // Data input from control system (for immediate instructions)
-    output [15:0] dOut, // Data output to memory 
-    input [15:0] dIn, // Data input from memory
-    output [15:0] addr,
-    input ready, // Indicates that memory is ready for the data exchange requested by the core
-    input writeRequest, // Indicates that the core wishes to write to memory
-    input readRequest, // Indicates that the core wishes to read from memory
-    input streamRequest, // Indicates that the core is requesting a frame stream
-	 input CLK
-    );
+module Core(opcode, regA, regB, regC, instrComplete, dInImmediate, dOut, dIn, addr, ready, writeRequest, readRequest, streamRequest, CLK);
+	input [2:0] opcode;
+	input [2:0] regA;
+	input [2:0] regB;
+	input [2:0] regC;
+	output instrComplete;
+	input [15:0] dInImmediate; // Data input from control system (for immediate instructions)
+	output [15:0] dOut; // Data output to memory 
+	input [15:0] dIn; // Data input from memory
+	output [15:0] addr;
+	input ready; // Indicates that memory is ready for the data exchange requested by the core
+	input writeRequest; // Indicates that the core wishes to write to memory
+	input readRequest; // Indicates that the core wishes to read from memory
+	input streamRequest; // Indicates that the core is requesting a frame stream
+	input CLK;
 
-
+	reg [2:0] register [15:0] ; // Local registers
+	reg [15:0] result;
+	reg [2:0] delayCounter;
+	wire [31:0] multResult;
+	wire [15:0] divResult, remainder;
+	
+	coreMultiplier cMult(CLK, regA, regB, multResult);
+	coreDivider dDiv(1'b1, CLK, regA, divResult, regA, remainder);
+	
+	// Several of the operations can run in combinatorial time, so why not let them.
+	always @(*)
+	begin
+		case(opcode)
+			3'b000: // Add
+				result = register[regA] + register[regB];
+			3'b001: // Sub
+				result = register[regA] - register[regB];
+			3'b100: // ShiftLeft
+				result = register[regA] << register[regB];
+			3'b101: // ShiftRight
+				result = register[regA] << register[regB];
+			default: result = result;
+		endcase
+	end
+	
+	always @ (posedge CLK)
+	begin
+		instrComplete <= (instrComplete == 1'b1) ? 0 : instrComplete; // Ensures that this only stays high for once cycle.
+	
+		case(opcode)
+			3'b010: // Mult, outputs after the first clock.
+				begin
+					if (delayCounter != 3'b001)
+						delayCounter <= delayCounter + 1'b1;
+					else
+						begin
+							delayCounter 	<= 0;
+							register[regC] 	<= multResult[15:0]; // Need some sort of solution for dealing with other 16-bits
+							instrComplete 	<= 1'b1;
+						end
+				end
+			3'b011: // Div, outputs after the first clock;
+				begin
+					if (delayCounter != 3'b001)
+						delayCounter 	<= delayCounter + 1'b1;
+					else
+					begin
+						delayCounter 	<= 0;
+						register[regC] 	<= divResult; // Need some sort of solution for dealing with remainder
+						instrComplete 	<= 1'b1;
+					end
+				end
+			
+			/* I'm probably going to need to talk to Aaron about this.
+				- How does this handle local block memory vs 
+				- Assuming load offers access to immediates, how is that to be indicated? 
+			//3'b110: // Load
+			//3'b111: // Store*/
+			default: 
+				begin
+					register[regC] 	<= result; // All those which are able to use combinatorial time.
+					delayCounter 	<= 0;
+					instrComplete 	<= 1'b1;
+				end
+		endcase	
+	end
 endmodule
